350a99c662ac521843cd0c51dcbf776eabdfeecf
tnmorenoadrian <tnmorenoadrian@gmail.com>
2022-12-17 14:45:38 -0300

    Long text Persona

--- src/main/java/com/yoprogramo/portfolio/model/Persona.java
+++ src/main/java/com/yoprogramo/portfolio/model/Persona.java
@@ -24,6 +24,7 @@ public class Persona {
     private String email;
     @Column(nullable = false, length = 64)
     private String password;
+    @Column(columnDefinition="TEXT")
     private String sobre_mi;
     private String ocupacion;
     private String image_background;
